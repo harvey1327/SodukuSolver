49432ad7f77e7ecf7867a045a0f24089b7bcc067
HARBAL <HARBAL@LUK-4YS4KC2.corp.capgemini.com>
2017-02-06 16:05:21 +0000

    Renamed Packages

--- src/main/java/com/provinggrounds/sodukusolver/controller/SodukuSolverController.java
+++ src/main/java/com/provinggrounds/sodukusolver/controller/SodukuSolverController.java
@@ -1,4 +1,4 @@
-package com.provinggrounds.controller;
+package com.provinggrounds.sodukusolver.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;

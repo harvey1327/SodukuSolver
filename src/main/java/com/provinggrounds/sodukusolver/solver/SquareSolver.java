89ff9b0e24cea93a69da05f328a48eeef89196ea
HARBAL <HARBAL@LUK-4YS4KC2.corp.capgemini.com>
2017-02-08 11:55:21 +0000

    Added Grid to be processed

--- src/main/java/com/provinggrounds/sodukusolver/solver/SquareSolver.java
+++ src/main/java/com/provinggrounds/sodukusolver/solver/SquareSolver.java
@@ -1,11 +1,16 @@
 package com.provinggrounds.sodukusolver.solver;
 
+import com.provinggrounds.sodukusolver.domain.Grid;
 import com.provinggrounds.sodukusolver.domain.GridSquare;
-
+/**
+ * Given any GridSquare we want to find out the parent square it resides in get all other GridSquares 
+ * @author HARBAL
+ *
+ */
 class SquareSolver implements SolverInterface {
 
 	@Override
-	public void process(GridSquare gridSquare) {
+	public void process(GridSquare gridSquare, Grid grid) {
 		System.out.println(this.getClass().getName()+gridSquare.getX()+","+gridSquare.getY());
 	}
 }

883507bb17cdf52ea7ab7be6d7b370367a0aa6f7
HARBAL <HARBAL@LUK-4YS4KC2.corp.capgemini.com>
2017-02-08 11:42:14 +0000

    Added Spring to SodukuSolverHandler - Solver Command is now prototype

--- src/main/java/com/provinggrounds/sodukusolver/handler/SodukuSolverHandler.java
+++ src/main/java/com/provinggrounds/sodukusolver/handler/SodukuSolverHandler.java
@@ -3,12 +3,17 @@ package com.provinggrounds.sodukusolver.handler;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
+
 import com.provinggrounds.sodukusolver.domain.Grid;
 import com.provinggrounds.sodukusolver.domain.GridSquare;
 import com.provinggrounds.sodukusolver.solver.SolverCommand;
 import com.provinggrounds.sodukusolver.solver.SolverCommandBuilder;
 
 public class SodukuSolverHandler {
+	
+	private ApplicationContext context = new FileSystemXmlApplicationContext("src/main/springBeanConfig/SodukuSolverHandler.xml");
 
 	public Grid createGrid(int size){
 		Grid grid = new Grid();
@@ -34,7 +39,7 @@ public class SodukuSolverHandler {
 	}
 	
 	private SolverCommand getSolverCommand(){
-		SolverCommand sc = new SolverCommandBuilder().buildSolverCommand();
+		SolverCommand sc = context.getBean("solverCommandBuilder",SolverCommandBuilder.class).buildSolverCommand();
 		return sc;
 	}
 }
